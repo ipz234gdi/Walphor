76ac192e206c0bcde1b9238322d47fe6173ade24
Nazarii Havryliuk <ipz234_gnp@student.ztu.edu.ua>
2025-02-02 17:44:13 +0200

    Refactored Card.cs to use Singleton pattern for global state

--- Walphor/Card.cs
+++ Walphor/Card.cs
@@ -2,24 +2,37 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Reflection;
-using System.Reflection.Emit;
-using System.Runtime.Remoting.Messaging;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Xml.Linq;
 
 namespace Walphor
 {
+    internal class CardManager
+    {
+        private static CardManager _instance;
+
+        public static CardManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new CardManager();
+                }
+                return _instance;
+            }
+        }
+
+        public List<Card> AllCards { get; } = new List<Card>();
+
+        private CardManager() { }
+    }
+
     internal class Card : GameObject
     {
-        private static List<Card> AllCards = new List<Card>();
         private TextBlock infoTextBlock;
         private TextBlock costTextBlock;
         private Border border;
@@ -33,6 +46,7 @@ namespace Walphor
         public bool IsClone { get; private set; } = false;
         public Canvas Canvas { get; set; }
         public int Index { get; set; }
+
         private Card(double x, double y, double width, double height, string name, double def, double atck, double algo, double health, double cost, string imageSource, Canvas canvas, string infoCard, bool isClone, int index)
         : base(x, y, width, height, imageSource, canvas)
         {
@@ -53,99 +67,37 @@ namespace Walphor
             IsClone = isClone;
             InfoCard = infoCard;
             Canvas = canvas;
-            AllCards.Add(this);
+
+            CardManager.Instance.AllCards.Add(this);
 
             if (!isClone)
             {
-                costTextBlock = new TextBlock
-                {
-                    Text = $"Цiна: {cost}",
-                    FontSize = 12,
-                    FontFamily = new FontFamily(new Uri("pack://application:,,,/"), "./Fonts/#F77 Minecraft"),
-                    Foreground = Brushes.Gold,
-                    Background = Brushes.Black,
-                    TextAlignment = TextAlignment.Center,
-                    VerticalAlignment = VerticalAlignment.Center,
-                    Width = width,
-                    Height = 28,
-                    Padding = new Thickness(0, 4, 0, 0)
-                };
-
-                Canvas.SetLeft(costTextBlock, x);
-                Canvas.SetTop(costTextBlock, y + height + 16);
-                Canvas.SetZIndex(costTextBlock, 2);
+                costTextBlock = CreateTextBlock($"Цiна: {cost}", x, y + height + 16, 12, Brushes.Gold);
                 canvas.Children.Add(costTextBlock);
             }
 
-            infoTextBlock = new TextBlock
-            {
-                Text = $"Iм'я: {Name}\n" +
-                       $"Оборона:  +{Def}\n" +
-                       $"Здоров'я: +{Health}\n" +
-                       $"Атака:        +{Atck}\n" +
-                       $"Алгоритм: +{Algo}\n" +
-                       $"Цiна:            {Cost}\n\n" +
-                       $"{InfoCard}",
-                FontSize = 16,
-                FontFamily = new FontFamily(new Uri("pack://application:,,,/"), "./Fonts/#F77 Minecraft"),
-                Foreground = Brushes.White,
-                TextWrapping = TextWrapping.Wrap,
-                Padding = new Thickness(10, 5, 10, 5),
-            };
-            double borderLeftX = x - 200;
-            if (x < 200)
-            {
-                borderLeftX = x + width;
-            }
-            double borderTopY = y + 0;
-            if (canvas.Height - y < height + 44)
-            {
-                borderTopY = y - 88;
-                if (borderTopY < 0) borderTopY = 0;
-            }
-
-            border = new Border
-            {
-                Width = 200,
-                Height = 300,
-                Child = infoTextBlock,
-                Background = new ImageBrush
-                {
-                    ImageSource = new BitmapImage(new Uri("pack://application:,,,/Data/Cards/Info.png")),
-                    Stretch = Stretch.Fill
-                },
-                Opacity = 1,
-                Visibility = System.Windows.Visibility.Hidden
-            };
-            Canvas.SetLeft(border, borderLeftX);
-            Canvas.SetZIndex(border, 3);
-            Canvas.SetTop(border, borderTopY);
+            infoTextBlock = CreateTextBlock(GetCardInfo(), x, y, 16, Brushes.White);
+            border = CreateInfoBorder(x, y, width, height);
             canvas.Children.Add(border);
         }
+
         public static Card Create(double x, double y, double width, double height, string name, double def, double atck, double algo, double health, double cost, string imageSource, Canvas canvas, string infoCard)
         {
-            return new Card(x, y, width, height, name, def, atck, algo, health, cost, imageSource, canvas, infoCard, false, 13);
+            return new Card(x, y, width, height, name, def, atck, algo, health, cost, imageSource, canvas, infoCard, false, 0);
         }
+
         private void Image_MouseLeftButtonDown(object sender, RoutedEventArgs e)
         {
-            Debug.WriteLine("Картка клiкнута!");
-            var card = sender as Image;
-            if (card != null)
-            {
-                Debug.WriteLine("Це зображення картки: " + (card.Tag as Card)?.Name);
-            }
+            Debug.WriteLine($"Картка {Name} клiкнута!");
         }
+
         private void Image_MouseEnter(object sender, MouseEventArgs e)
         {
             if (!Shop.isSelectingCards)
             {
-                var image = sender as Image;
-                if (image != null)
-                {
-                    image.Opacity = 0.8;
-                    infoTextBlock.Visibility = System.Windows.Visibility.Visible;
-                    border.Visibility = System.Windows.Visibility.Visible;
-                }
+                image.Opacity = 0.8;
+                infoTextBlock.Visibility = Visibility.Visible;
+                border.Visibility = Visibility.Visible;
             }
         }
 
@@ -153,37 +105,26 @@ namespace Walphor
         {
             if (!Shop.isSelectingCards)
             {
-                var image = sender as Image;
-                if (image != null)
-                {
-                    image.Opacity = 1.0;
-                    infoTextBlock.Visibility = System.Windows.Visibility.Hidden;
-                    border.Visibility = System.Windows.Visibility.Hidden;
-                }
+                image.Opacity = 1.0;
+                infoTextBlock.Visibility = Visibility.Hidden;
+                border.Visibility = Visibility.Hidden;
             }
         }
+
         public static void ClearByName(string name, Canvas canvas)
         {
-            List<Card> toRemove = new List<Card>();
-            foreach (var card in AllCards)
-            {
-                if (card.Name == name)
-                {
-                    canvas.Children.Remove(card.image);
-                    toRemove.Add(card);
-                    Debug.WriteLine("Картка видалена" + card.Name);
-                }
-            }
+            var toRemove = CardManager.Instance.AllCards.Where(card => card.Name == name).ToList();
             foreach (var card in toRemove)
             {
-                AllCards.Remove(card);
+                canvas.Children.Remove(card.image);
+                CardManager.Instance.AllCards.Remove(card);
+                Debug.WriteLine($"Картка {card.Name} видалена");
             }
         }
 
         public static void MoveCardToCanvas(string cardName, Canvas newCanvas, double newX, double newY)
         {
-            // Знаходимо всi картки з заданим iменем
-            var cardsToMove = AllCards.Where(card => card.Name == cardName).ToList();
+            var cardsToMove = CardManager.Instance.AllCards.Where(card => card.Name == cardName).ToList();
 
             Application.Current.Dispatcher.Invoke(() =>
             {
@@ -195,17 +136,74 @@ namespace Walphor
                     }
 
                     card.Canvas = newCanvas;
-
                     Canvas.SetLeft(card.image, newX);
                     Canvas.SetTop(card.image, newY);
-
                     newCanvas.Children.Add(card.image);
                 }
             });
         }
+
         public override GameObject Clone(Canvas newCanvas, double x, double y, string name, int index)
         {
             return new Card(x, y, Width, Height, name, Def, Atck, Algo, Health, Cost, ImageSource, newCanvas, InfoCard, true, index);
+        }
+
+        private TextBlock CreateTextBlock(string text, double x, double y, int fontSize, Brush color)
+        {
+            var textBlock = new TextBlock
+            {
+                Text = text,
+                FontSize = fontSize,
+                FontFamily = new FontFamily(new Uri("pack://application:,,,/"), "./Fonts/#F77 Minecraft"),
+                Foreground = color,
+                Background = Brushes.Black,
+                TextAlignment = TextAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                Width = Width,
+                Height = 28,
+                Padding = new Thickness(0, 4, 0, 0)
+            };
+
+            Canvas.SetLeft(textBlock, x);
+            Canvas.SetTop(textBlock, y);
+            Canvas.SetZIndex(textBlock, 2);
+            return textBlock;
+        }
+
+        private Border CreateInfoBorder(double x, double y, double width, double height)
+        {
+            double borderLeftX = x < 200 ? x + width : x - 200;
+            double borderTopY = (Canvas.Height - y < height + 44) ? y - 88 : y;
+
+            var border = new Border
+            {
+                Width = 200,
+                Height = 300,
+                Child = infoTextBlock,
+                Background = new ImageBrush
+                {
+                    ImageSource = new BitmapImage(new Uri("pack://application:,,,/Data/Cards/Info.png")),
+                    Stretch = Stretch.Fill
+                },
+                Opacity = 1,
+                Visibility = Visibility.Hidden
+            };
+
+            Canvas.SetLeft(border, borderLeftX);
+            Canvas.SetZIndex(border, 3);
+            Canvas.SetTop(border, borderTopY);
+            return border;
+        }
+
+        private string GetCardInfo()
+        {
+            return $"Iм'я: {Name}\n" +
+                   $"Оборона: +{Def}\n" +
+                   $"Здоров'я: +{Health}\n" +
+                   $"Атака: +{Atck}\n" +
+                   $"Алгоритм: +{Algo}\n" +
+                   $"Цiна: {Cost}\n\n" +
+                   $"{InfoCard}";
         }
     }
 }
